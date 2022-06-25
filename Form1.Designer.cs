66c2474337e55c5555aae9a0eb441134d72304b1
akoc1 <72667213+akoc1@users.noreply.github.com>
2022-06-25 14:15:13 +0300

    project folder

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Note_Taking_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Save = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Note = new System.Windows.Forms.RichTextBox();
+            this.Load = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.New = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Save
+            // 
+            this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Save.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Save.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Save.FlatAppearance.BorderSize = 0;
+            this.Save.FlatAppearance.MouseDownBackColor = System.Drawing.Color.RoyalBlue;
+            this.Save.FlatAppearance.MouseOverBackColor = System.Drawing.Color.CornflowerBlue;
+            this.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save.Font = new System.Drawing.Font("Nirmala UI", 12.75F);
+            this.Save.ForeColor = System.Drawing.Color.White;
+            this.Save.Image = ((System.Drawing.Image)(resources.GetObject("Save.Image")));
+            this.Save.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Save.Location = new System.Drawing.Point(261, 370);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(198, 43);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Delete.BackColor = System.Drawing.Color.IndianRed;
+            this.Delete.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Delete.FlatAppearance.BorderSize = 0;
+            this.Delete.FlatAppearance.MouseDownBackColor = System.Drawing.Color.IndianRed;
+            this.Delete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCoral;
+            this.Delete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Delete.Font = new System.Drawing.Font("Nirmala UI", 12.75F);
+            this.Delete.ForeColor = System.Drawing.Color.White;
+            this.Delete.Image = ((System.Drawing.Image)(resources.GetObject("Delete.Image")));
+            this.Delete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Delete.Location = new System.Drawing.Point(484, 370);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(188, 43);
+            this.Delete.TabIndex = 2;
+            this.Delete.Text = "    Delete";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Title
+            // 
+            this.Title.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
+            this.Title.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Title.Font = new System.Drawing.Font("Nirmala UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(16, 34);
+            this.Title.Multiline = true;
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(443, 30);
+            this.Title.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(11, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Note";
+            // 
+            // Note
+            // 
+            this.Note.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Note.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
+            this.Note.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Note.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Note.ForeColor = System.Drawing.Color.White;
+            this.Note.Location = new System.Drawing.Point(16, 95);
+            this.Note.Name = "Note";
+            this.Note.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.Note.Size = new System.Drawing.Size(443, 269);
+            this.Note.TabIndex = 7;
+            this.Note.Text = "";
+            // 
+            // Load
+            // 
+            this.Load.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Load.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(70)))));
+            this.Load.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Load.ForeColor = System.Drawing.Color.White;
+            this.Load.FormattingEnabled = true;
+            this.Load.ItemHeight = 30;
+            this.Load.Location = new System.Drawing.Point(484, 34);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(188, 330);
+            this.Load.TabIndex = 8;
+            this.Load.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(479, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 30);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Load";
+            // 
+            // New
+            // 
+            this.New.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.New.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.New.Cursor = System.Windows.Forms.Cursors.Default;
+            this.New.FlatAppearance.BorderSize = 0;
+            this.New.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkSeaGreen;
+            this.New.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.New.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.New.Font = new System.Drawing.Font("Nirmala UI", 12.75F);
+            this.New.ForeColor = System.Drawing.Color.White;
+            this.New.Image = ((System.Drawing.Image)(resources.GetObject("New.Image")));
+            this.New.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.New.Location = new System.Drawing.Point(16, 370);
+            this.New.Name = "New";
+            this.New.Size = new System.Drawing.Size(198, 43);
+            this.New.TabIndex = 10;
+            this.New.Text = "New";
+            this.New.UseVisualStyleBackColor = false;
+            this.New.Click += new System.EventHandler(this.New_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(684, 424);
+            this.Controls.Add(this.New);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Note);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Save);
+            this.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(700, 463);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Temporary Note Taking App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.TextBox Title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox Note;
+        private System.Windows.Forms.ListBox Load;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button New;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_Taking_App
+{
+    public partial class Form1 : Form
+    {
+        List<string> titleTextList = new List<string>();
+        List<string> noteTextList = new List<string>();
+
+        int currentItemCounter = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Title.Text == "" && Note.Text == "")
+                MessageBox.Show("Please fill the 'Title' and 'Note' area.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else if (Title.Text == "")
+                MessageBox.Show("Please fill the 'Title' area.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else if (Note.Text == "")
+                MessageBox.Show("Please fill the 'Note' area.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                listAdder();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Load.SelectedIndex != -1)
+            {
+                Title.Text = titleTextList[Load.SelectedIndex];
+                Note.Text = noteTextList[Load.SelectedIndex];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Load.SelectedIndex != -1)
+            {
+                titleTextList.RemoveAt(Load.SelectedIndex);
+                noteTextList.RemoveAt(Load.SelectedIndex);
+                Load.Items.Remove(Load.Items[Load.SelectedIndex]);
+                currentItemCounter -= 1;
+
+                if (Load.Items.Count == 0)
+                {
+                    Title.Clear();
+                    Note.Clear();
+                }
+            }
+        }
+
+        private void New_Click(object sender, EventArgs e)
+        {
+            if (Note.TextLength > 0)
+            {
+                DialogResult sonuc = MessageBox.Show("Do you want to delete current note?", "Information", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (sonuc == DialogResult.Yes)
+                {
+                    Note.Clear();
+                    Title.Clear();
+                }
+            }
+        }
+
+        private void listAdder()
+        {
+            currentItemCounter += 1;
+
+            titleTextList.Add(Title.Text);
+            noteTextList.Add(Note.Text);
+            Load.Items.Add(titleTextList[currentItemCounter]);
+        }
+    }
+}
